e2053bea0deb1438cc12c8a53e347a768309ed29
mokaram00 <mk-zein@hotmail.com>
2025-05-06 04:09:03 +0300

    Update main.py

--- api/main.py
+++ api/main.py
@@ -1,1 +1,8 @@
-print("hi world")
+from http import HTTPStatus
+
+def handler(request):
+    print("hi world")  # هذا سيظهر في سجلات Vercel
+    return {
+        "statusCode": HTTPStatus.OK,
+        "body": "Hello from /api/main"
+    }
